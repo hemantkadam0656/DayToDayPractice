ce22b104e772313c0678853d4c9886cc90f320c2
Hemant Kadam <140627846+hemantkadam0656@users.noreply.github.com>
2025-04-03 23:40:47 +0530

    added Two Sum code

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,14 @@
+list = [2,7,11,15] 
 
+target = 9
+
+dict = {}
+
+for i, num in enumerate(list):
+    if num in dict:
+        print(dict[num], i)
+    else:
+        dict[target - num] = i
+
+
+print(dict)
