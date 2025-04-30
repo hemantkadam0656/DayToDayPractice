f026c9bdabe546a6516083e1c948e0285e4d4007
Hemant Kadam <140627846+hemantkadam0656@users.noreply.github.com>
2025-05-01 00:08:12 +0530

    added some more steps for linked list

--- DSA/LinkedList/FrontInsertion.py
+++ DSA/LinkedList/FrontInsertion.py
@@ -8,7 +8,9 @@ class Node():
 class LinkedList():
     def __init__(self):
         self.head = None
-        
+
+# insert at beggining 
+       
     def insert_at_front(self, new_data):
         new_node = Node(new_data, self.head)
         self.head = new_node
@@ -25,7 +27,9 @@ class LinkedList():
             itr = itr.next   
 
         print(llist + 'None')
+        
 
+# List insersion at the end or if head is empty the return list 
 
     def insert_at_end(self, new_data):
         if self.head is None:
@@ -37,8 +41,53 @@ class LinkedList():
             itr = itr.next
         
         itr.next = Node(new_data, None)
+
+# function to find length of linked list
     
+    def get_length(self):
+        count = 0
+        itr = self.head 
+        while itr:
+            count +=1
+            itr = itr.next 
+        return count  
+    
+# function to remove specific node or element from linked list 
+    def removeEle(self,index):
+        if index < 0 or index >= self.get_length():
+            raise Exception('Invalid index passed for list')
+
+        if index ==0:
+            return self.head
         
+        count = 0 
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+# Insert at specific position in linked list or at given position
+    def Insert_at(self, index, data):
+        if index < 0 or index >= self.get_length():
+            raise Exception('Index Invalid')
+        
+        if index == 0:
+            return self.insert_at_front(data)
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            count += 1
+            itr = itr.next 
+
+
 
 if __name__ == '__main__':
     list = LinkedList()
@@ -46,4 +95,6 @@ if __name__ == '__main__':
     list.insert_at_front(6)
     list.insert_at_end(4)
     list.insert_at_end(3)
+    list.print()
+    list.Insert_at(7,8)
     list.print()
