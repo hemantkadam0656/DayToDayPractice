88b174279ae1d2b3caf46c8d8ecaecf7c2170af2
Hemant Kadam <140627846+hemantkadam0656@users.noreply.github.com>
2025-05-04 12:02:45 +0530

    added reverse linked list

--- DSA/Test.py
+++ DSA/Test.py
@@ -1,2 +1,33 @@
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+    
+
+class linkedList():
+    def __init__(self):
+        self.head = None
+    
+    def inser_at_beggining(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+    
+    def print(self):
+        if self.head is None:
+            print('List is Empty')
+            return 
+        
+        itr = self.head
+        list = ''
+        while itr:
+            list += str(itr.data) + ' -- '
+            itr = itr.next
+        
+        print(list + 'None')
 
 
+if __name__ == '__main__':
+    ll = linkedList()
+    ll.inser_at_beggining(1)
+    ll.inser_at_beggining(2)
+    ll.print()
